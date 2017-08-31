899cc1496eebb30c607666126cb0b52a45be6af7
camelspotter <t.parisinos@gmail.com>
2017-08-31 12:11:30 +0300

    added clip method

--- include/string.hpp
+++ include/string.hpp
@@ -112,6 +112,8 @@ public:
 
 	virtual string& clear();
 
+	virtual string& clip(u32);
+
 	virtual i32 compare(const string&, bool = false) const;
 
 	virtual bool equals(const string&, bool = false) const;
